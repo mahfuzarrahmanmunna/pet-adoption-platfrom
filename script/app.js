192db16d8ad65560120f7a4f7fbdf3cecece3d55
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-03-19 16:01:50 +0600

    added new js file

--- script/app.js
+++ script/app.js
@@ -18,13 +18,60 @@ const displayCategory = (categories) => {
         </button>
         `
         categoryContainer.appendChild(newDiv)
-        console.log(category)
+        // console.log(category)
     });
 }
 
 
 const loadPets = async (categoryName) => {
+    spinnerShow('spiner')
     const response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${categoryName}`)
     const data = await response.json()
-    console.log(data.data)
+    if (data.data) {
+        displayPets(data.data)
+        spinnerHide('spiner')
+    }
 }
+
+const displayPets = (pets) => {
+    const petsContainer = document.getElementById('petsContainer');
+    petsContainer.innerHTML = ''
+    if (pets.length == 0) {
+        petsContainer.innerHTML = `
+            <h1 id="status" class="text-center text-red-500">NO data Fund !!!!!!!!!</h1>
+
+        `
+    }
+    pets.forEach(pet => {
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+           <div class="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img class=''
+                    src=${pet.image}
+                    alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">${pet.breed}</h2>
+                    <p>
+                        ${pet.pet_details}
+                    </p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        `
+        petsContainer.append(newDiv)
+        console.log(pet)
+    })
+}
+
+const spinnerHide = (id) => {
+    document.getElementById(id).style.display = 'none'
+}
+
+const spinnerShow = (id) => {
+    document.getElementById(id).style.display = 'block'
+}
+
